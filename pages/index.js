e0fca65d8ebd2ded7f195b93ef5d5491a79b2a61
songji1165 <songji1165@gmail.com>
2022-02-03 22:39:33 +0900

    Fetch api 시 env , nextjs rewrites(masking) 활용

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,51 @@
-import Head from "next/head"
 import Seo from "../components/Seo";
+import {useEffect, useState} from "react";
 
-export default function Home(){
-    return(
-        <div>
-            {/*<Head>*/}
-            {/*    <title>Home | Next Movies</title>*/}
-            {/*</Head>*/}
+
+export default function Home() {
+    const [movies, setMovies] = useState([]);
+    useEffect(() => {
+        (async () => {
+            const {results} = await (await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)).json();
+            console.log("DATA : ", results);
+            if (results) {
+                setMovies(results);
+            }
+        })();
+    }, []);
+    return (
+        <div className="container">
             <Seo title="home"/>
-            <h1>Hi</h1>
-        </div>
-    );
+            <div>
+                {!movies && <h4>Loading...</h4>}
+                {movies?.map((movie, idx) => {
+                    return (<div className="movie" key={movie.id}>
+                        <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}/>
+                        <h4>{movie.original_title}</h4>
+                    </div>);
+                })}
+            </div>
+            <style jsx>{`
+            .container {
+              display: grid;
+              grid-template-columns: 1fr 1fr;
+              padding: 20px;
+              gap: 20px;
+            }
+            .movie img {
+              max-width: 100%;
+              border-radius: 12px;
+              transition: transform 0.2s ease-in-out;
+              box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+            }
+            .movie:hover img {
+              transform: scale(1.05) translateY(-10px);
+            }
+            .movie h4 {
+              font-size: 18px;
+              text-align: center;
+            }
+      `}
+            </style>
+        </div>);
 }
