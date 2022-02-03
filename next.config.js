5d15920513f01c56521760e92b4176ee687e46b9
songji1165 <songji1165@gmail.com>
2022-02-03 22:38:39 +0900

    next.config.js 활용 방법

--- next.config.js
+++ next.config.js
@@ -1,3 +1,33 @@
+const API_KEY = process.env.API_KEY;
+// const API_KEY = "5481b26a7f45662cd735f1c07167cccd";
+
 module.exports = {
   reactStrictMode: true,
+  async redirects(){
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent : false, // 브라우저나 검색엔진이 이 브라우저를 기억하는지 결정하는 여부
+      },
+      {
+        source: "/old-blog/:path",
+        destination: "/new-blog/:path",
+        permanent : false,
+      },
+      {
+        source: "/old-blog2/:path*",
+        destination: "/new-blog2/:path*",
+        permanent : false,
+      }
+    ]
+  },
+  async rewrites(){
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      }
+    ]
+  }
 }
